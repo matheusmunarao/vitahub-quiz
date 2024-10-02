cd7daf14eef635b94c035e177f68e92a8af355a4
Matheus Munarão <62369374+matheusmunarao@users.noreply.github.com>
2024-10-02 11:49:52 -0300

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -4,6 +4,8 @@ const appConfig = {
   TEMPERATURE: 0.7,
   MODEL: 'gpt-3.5-turbo',
   OPENAI_API_KEY: import.meta.env.VITE_REACT_APP_OPENAI_API_KEY || '',
+  OPENAI_ORGANIZATION_ID: process.env.OPENAI_ORGANIZATION_ID,
+  OPENAI_PROJECT_ID: process.env.OPENAI_PROJECT_ID,
 };
 
 export default appConfig;
